100b6b5e3329d697d22f2a82c0fdb2d2a061af24
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2025-09-08 11:45:37 +0200

    Refactor ProductPage component to render ProductList instead of single ProductCard and update styles accordingly

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,11 +1,11 @@
-import ProductCard from "../../components/ProductCard/ProductCard";
+import ProductList from "../../components/ProductList/ProductList";
 import styles from "./ProductPage.module.css";
 
 export default function ProductPage() {
   return (
     <main className={styles.pageContainer}>
       <h1 className={`text-preset-1 ${styles.pageHeading}`}>Desserts</h1>
-      <ProductCard />
+      <ProductList />
     </main>
   );
 }
